c445e7e69f7f3a34da0c42cd62d7ab4103b1c39e
Dzeneralen <3482008+Dzeneralen@users.noreply.github.com>
2023-12-13 23:01:14 +0100

    day 6 part 2

--- day6/day6.fsx
+++ day6/day6.fsx
@@ -7,7 +7,6 @@ let lines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "/input.txt")
 let getNumbers (items: string array) =
     items
     |> Array.filter (fun x -> fst (System.Int32.TryParse x))
-    |> Array.map int
 
 let times =
     lines[0].Split " "
@@ -17,15 +16,32 @@ let distances =
     lines[1].Split " "
     |> getNumbers
 
-let options = 
+let getOptions (times: string array) (distances: string array) = 
     Array.zip times distances
-    |> Array.map (fun (time, distance) -> 
-        Array.init time (fun x -> (x, time - x))
+    |> Array.map (fun (t, d) -> (int t, int64 d))
+    |> Array.map (fun (t, d) -> 
+        Array.init t (fun x -> (x, t - x))
         |> Array.map (fun (speed, timeLeft) -> 
-            speed * timeLeft
+            int64 speed * int64 timeLeft
         )
-        |> Array.filter (fun x -> x > distance)
+        |> Array.filter (fun x -> x > d)
         |> Array.length
     )
 
-let result = options |> Array.fold (*) 1
+let result =
+    getOptions times distances
+    |> Array.fold (*) 1
+
+// PART 2
+
+let parseLineAsSingleDigit (line: string) =
+    line.Split " "
+    |> getNumbers
+    |> Array.fold (fun acc next -> 
+        acc + string next
+    ) ""
+
+let time = lines[0] |> parseLineAsSingleDigit
+let distance = lines[1] |> parseLineAsSingleDigit
+
+let result2 = getOptions [|time|] [|distance|]
